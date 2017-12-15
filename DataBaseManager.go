1a0889928f715e8ce4a3e16ae0fee0e72435357a
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-15 04:12:56 +0300

    DataBase Connection

---
+++ DataBaseManager.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"time"
+)
+
+type DataBaseManager struct {
+	dbPath string
+}
+
+func (db *DataBaseManager) GetAllFiles() []os.FileInfo {
+	files, err := ioutil.ReadDir(db.dbPath)
+
+	if err != nil {
+		return nil
+	}
+
+	return files
+}
+
+func (db *DataBaseManager) UserIsExist(userId string) bool {
+	usersFiles := db.GetAllFiles()
+
+	if usersFiles == nil {
+		return false
+	}
+
+	for _, file := range usersFiles {
+		if file.Name() == userId {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (db *DataBaseManager) RegisterNewUser(userId string, nickname string, language string) (string, error) {
+	userData := "{"
+	userData += "\"player_id\":\"" + userId + "\","
+	userData += "\"player_nickname\":\"" + nickname + "\","
+	userData += "\"language\":\"" + language + "\","
+	userData += "\"created_date\":\"" + time.Now().Format(time.Stamp) + "\","
+	userData += "\"player_talks\":[]}"
+
+	// err := ioutil.WriteFile(db.dbPath+userId, []byte(userData), 0644)
+
+	return userData, nil
+}
