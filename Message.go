1468cf54f0463df3a29020e7bceba9f1018c073a
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-16 23:52:27 +0300

    Message initialize

--- Message.go
+++ Message.go
@@ -1,9 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type Message struct {
-	id       int
+	id       int64
 	body     string
-	sendData string
+	sendDate string
 	senderId string
-	talkId   int
+	talkId   int64
+}
+
+func (message *Message) Init(messageData map[string]interface{}) {
+
+	idString := fmt.Sprintf("%v", messageData["message_id"])
+	message.id, _ = strconv.ParseInt(idString, 10, 64)
+
+	message.body = fmt.Sprintf("%v", messageData["body"])
+	message.sendDate = fmt.Sprintf("%v", messageData["send_date"])
+	message.senderId = fmt.Sprintf("%v", messageData["sender_id"])
+
+	talkIdString := fmt.Sprintf("%v", messageData["talk_id"])
+	message.talkId, _ = strconv.ParseInt(talkIdString, 10, 64)
+	fmt.Println(message.id, message.body)
 }
