1ec5d78ff62aff25549ec3166793d80a2e4f3fc0
Umutcan Erturk <umutcan.erturk@ogr.sakarya.edu.tr>
2017-12-17 16:47:02 +0300

    Find Conversation

--- ExampleServer.go
+++ ExampleServer.go
@@ -1,25 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var arwServer *ARWServer
 var db *DataBaseManager
 
+const (
+	GetUserData          = "GetUserData"
+	SendMessage          = "SendMessage"
+	FindConversation     = "FindConversation"
+	FindedConversation   = "FindedConversation"
+	CannotFindActiveUser = "CannotFindActiveUser"
+)
+
 func main() {
 	arwServer = new(ARWServer)
 	db = new(DataBaseManager)
-	db.dbPath = "ServerFiles/"
+	db.dbPath = "ServerFiles/Database/"
+	db.InitAllDb()
 
-	arwServer.AddEventHandler(&(arwServer.events.Login), Login_Event_Handler)
-	arwServer.AddExtensionHandler("GetUserData", GetUserDataHandler)
+	arwServer.AddExtensionHandler(GetUserData, GetUserDataHandler)
+	arwServer.AddExtensionHandler(SendMessage, SendMessageHandler)
+	arwServer.AddExtensionHandler(FindConversation, FindConversationHandler)
 	arwServer.Initialize()
 
 	arwServer.ProcessEvents()
-}
-
-func Login_Event_Handler(arwObj ARWObject) {
-
-	// user, _ := arwObj.GetUser(arwServer)
 }
 
 func GetUserDataHandler(server *ARWServer, user *ARWUser, arwObj ARWObject) {
@@ -28,7 +35,7 @@ func GetUserDataHandler(server *ARWServer, user *ARWUser, arwObj ARWObject) {
 	language, _ := arwObj.GetString("language")
 
 	if !db.UserIsExist(player_id) {
-		userData, err := db.RegisterNewUser(player_id, player_nickname, language)
+		userData, err := db.RegisterNewUser(player_id, player_nickname, language, user)
 
 		var obj ARWObject
 		obj.PutString("player_data", userData)
@@ -40,9 +47,10 @@ func GetUserDataHandler(server *ARWServer, user *ARWUser, arwObj ARWObject) {
 
 		x, _ := obj.GetString("player_data")
 		fmt.Println(x)
-		server.SendExtensionRequest("GetUserData", user, obj)
+		server.SendExtensionRequest(GetUserData, user, obj)
 	} else {
-		userData, err := db.GetUserData(player_id)
+		userData, player, err := db.GetUserData(player_id)
+		player.arwUser = user
 
 		var obj ARWObject
 		obj.PutString("player_data", userData)
@@ -52,6 +60,37 @@ func GetUserDataHandler(server *ARWServer, user *ARWUser, arwObj ARWObject) {
 			obj.PutString("error", "")
 		}
 
-		server.SendExtensionRequest("GetUserData", user, obj)
+		server.SendExtensionRequest(GetUserData, user, obj)
 	}
+}
+
+func FindConversationHandler(server *ARWServer, user *ARWUser, arwObj ARWObject) {
+	activeUsers := db.GetActiveUsers()
+
+	if len(activeUsers) == 0 {
+		//There is no player
+		// Send CannotFindConversation Request
+		return
+	}
+
+	owner := db.FindUserByARWUser(user)
+	findedUser := activeUsers[0]
+
+	var arwObject ARWObject
+	arwObject.PutString("player_nickname", findedUser.nickname)
+	arwObject.PutString("player_id", findedUser.id)
+
+	arwServer.SendExtensionRequest(FindedConversation, user, arwObject)
+
+	if findedUser.arwUser != nil {
+		var obj ARWObject
+		obj.PutString("player_nickname", owner.nickname)
+		obj.PutString("player_id", owner.id)
+
+		arwServer.SendExtensionRequest(FindedConversation, findedUser.arwUser, obj)
+	}
+}
+
+func SendMessageHandler(server *ARWServer, user *ARWUser, arwObj ARWObject) {
+
 }
